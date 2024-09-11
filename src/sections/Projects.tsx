7c2266dca1bfd4c571a98ba93a27e38849e1444f
MUHMMAD SALEH <khanmohdsaleh2002@gmail.com>
2024-09-11 13:15:35 +0530

    feat:add Tape section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -47,7 +47,7 @@ const portfolioProjects = [
 
 export const ProjectsSection = () => {
   return (
-    <section className="pb-16">
+    <section className="pb-16 lg:py-24">
       <div className="container">
         <div className="flex justify-center">
           <p className="uppercase font font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
@@ -57,49 +57,61 @@ export const ProjectsSection = () => {
         <h2 className="font-serif text-3xl mt-6 text-center">
           Featured Projects
         </h2>
-        <p className="text-white/60 mt-4 text-center">
+        <p className="text-white/60 mt-4 max-w-md mx-auto text-center md:text-lg lg:text-xl">
           See how I transformed concepts into engaging digital experiences.
         </p>
 
-        <div className="flex flex-col mt-10 gap-20">
+        <div className="flex flex-col mt-10 md:mt-20 gap-20">
           {portfolioProjects.map((project) => (
             <div
               key={project.title}
               className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10
-            after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 after:pointer-events-none"
+            after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10  lg:pt-16 lg:px-20 after:pointer-events-none"
             >
               <div
                 className="absolute inset-0 -z-10 opacity-5 "
                 style={{ backgroundImage: `url(${grainImage.src})` }}
               ></div>
-              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 text-transparent bg-clip-text font-bold uppercase tracking-widest text-sm ">
-                <span>{project.company}</span>
-                <span>&bull;</span>
-                <span>{project.year}</span>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 text-transparent bg-clip-text font-bold uppercase tracking-widest text-sm ">
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+                  <h3 className="font-serif text-2xl md:text-4xl mt-2 md:mt-5  ">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 mt-4 md:mt-5" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result) => (
+                      <li className=" flex  gap-2 text-sm text-white/50 md:text-base">
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+
+                  <a href={project.link}>
+                    <button className="bg-white h-12 w-full md:w-auto rounded-xl text-gray-950 font-semibold inline-flex items-center justify-center mt-8 -mb-4 px-6 md:-md-0 ">
+                      <span>Visit Live Site</span>
+                      <ArrowUpRightIcon />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    className="mt-8 -mb-4 md:-mb-0  lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none"
+                    src={project.image}
+                    alt={project.title}
+                  />
+                </div>
               </div>
-              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
-              <hr className="border-t-2 border-white/5 mt-4" />
-              <ul className="flex flex-col gap-4 mt-4">
-                {project.results.map((result) => (
-                  <li className=" flex  gap-2 text-sm text-white/50">
-                    <CheckCircleIcon className="size-5" />
-                    <span>{result.title}</span>
-                  </li>
-                ))}
-              </ul>
-
-              <a href={project.link}>
-                <button className="bg-white h-12 w-full rounded-xl text-gray-950 font-semibold inline-flex items-center justify-center mt-8 -mb-4 ">
-                  <span>Visit Live Site</span>
-                  <ArrowUpRightIcon />
-                </button>
-              </a>
-
-              <Image className="mt-8" src={project.image} alt={project.title} />
             </div>
           ))}
         </div>
       </div>
     </section>
+    
   );
 };
--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,34 @@
+import StarIcon from "../assets/icons/star.svg";
+const words = [
+  "Performance",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "Maintainable",
+  "Responsive",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div>
+      <div className="py-16 lg:py-24 overflow-x-clip ">
+        <div className="bg-gradient-to-r from-emerald-300 to-sky-400  -rotate-3 -mx-1">
+          <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] ">
+            <div className=" flex flex-none gap-4 py-3">
+              {words.map((word) => (
+                <div key={word} className="inline-flex gap-4 items-center text-gray-900 font-extrabold text-sm uppercase ">
+                  <span>{word}</span>
+                  <StarIcon ClassName="size-6 -rotate-12" />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
