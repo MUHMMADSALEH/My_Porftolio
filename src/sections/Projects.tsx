dca1ca8d9d219a769d53a654d7460fcae3fe11ac
MUHMMADSALEH <khanmohdsaleh2002@gmail.com>
2024-09-29 00:09:20 +0530

    feat:add animations to Hero,Projects,Tape components

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import CheckCircleIcon from "../assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "../assets/icons/arrow-up-right.svg";
 import grainImage from "../assets/images/grain.jpg";
+import { Card } from "@/components/Card";
 
 const portfolioProjects = [
   {
@@ -62,12 +63,11 @@ export const ProjectsSection = () => {
         </p>
 
         <div className="flex flex-col mt-10 md:mt-20 gap-20">
-          {portfolioProjects.map((project) => (
-            <div
+          {portfolioProjects.map((project, projectIndex) => (
+            <Card
               key={project.title}
-              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10
-            after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10  lg:pt-16 lg:px-20 after:pointer-events-none"
-            >
+              className=" px-8 pt-8 md:pt-12 md:px-10  lg:pt-16 lg:px-20  sticky "
+              style={{ top: `calc(64px+ ${projectIndex * 40}px)` }}>
               <div
                 className="absolute inset-0 -z-10 opacity-5 "
                 style={{ backgroundImage: `url(${grainImage.src})` }}
@@ -107,11 +107,11 @@ export const ProjectsSection = () => {
                   />
                 </div>
               </div>
-            </div>
+            </Card>
           ))}
         </div>
       </div>
     </section>
-    
+
   );
 };
